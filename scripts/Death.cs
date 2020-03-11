4b172708693b8a63c7d67e11d8f1440c93974f4c
RusgyCufu <RusgyCufu@users.noreply.github.com>
2020-03-11 17:38:08 +0300

    Push
    Oof

---
+++ scripts/Death.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Death : MonoBehaviour
+{
+    [SerializeField] bool hide = false;
+    private GameObject main;
+    private GameObject restartScreen;
+
+    private Color oldColor;
+
+    GameObject GetChildWithName(GameObject obj, string name)
+    {
+        Transform trans = obj.transform;
+        Transform childTrans = trans.Find(name);
+        if (childTrans != null)
+        {
+            return childTrans.gameObject;
+        }
+        else
+        {
+            return null;
+        }
+    }
+        
+    void Start()
+    {
+        main = (GameObject)GameObject.FindGameObjectsWithTag("main").GetValue(0);
+        restartScreen = (GameObject)GameObject.FindGameObjectsWithTag("Restart").GetValue(0);
+
+        oldColor = gameObject.GetComponent<SpriteRenderer>().color;
+        if (hide)
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(0f, 0f, 0f, 0f);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision == main.GetComponent<CapsuleCollider2D>())
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = oldColor;
+            restartScreen.GetComponent<Canvas>().enabled = true;
+            AudioManager.AudioManager.m_instance.PlaySFX(2);
+            Time.timeScale = 0;
+        }
+    }
+}
---
+++ scripts/OneZero.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OneZero : MonoBehaviour
+{
+    [SerializeField] public int value;
+    [SerializeField] public GameObject displayText;
+    [SerializeField] public UnityEvent OnChange;
+
+    private GameObject main;
+
+    void Start()
+    {
+        main = (GameObject)GameObject.FindGameObjectsWithTag("main").GetValue(0);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision == main.GetComponent<CapsuleCollider2D>())
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = new Vector4(0, 0, 0, 0);
+            if (value == 0)
+            {
+                value = 1;
+            }
+            else if(value == 1)
+            {
+                value = 0;
+            }
+            displayText.GetComponent<TMPro.TextMeshProUGUI>().text = value.ToString();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision == main.GetComponent<CapsuleCollider2D>())
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = new Vector4(1, 1, 1, 1);
+        }
+    }
+}
