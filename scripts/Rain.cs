621bc3080a944ef6fda8cec46d98b3f67a522e55
Wgmlgz <62064707+Wgmlgz@users.noreply.github.com>
2020-04-29 18:31:41 +0300

    ККК

---
+++ scripts/Rain.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rain : MonoBehaviour
+{
+    GameObject cam;
+    void Start()
+    {
+        cam = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float t = transform.position.z;
+        //Vector3 tmp = cam.transform.position;
+        //tmp.z = t;
+        //transform.position = tmp;
+        //GetComponent<ParticleSystem>().po
+    }
+}
